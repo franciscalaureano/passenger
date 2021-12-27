3aba41abed7133fa2e0654bf3a04bfae2a8d473c
mrjgsantos <58551734+mrjgsantos@users.noreply.github.com>
2021-12-27 17:40:27 +0000

    Funcional

---
+++ js/src/signup.js
@@ -0,0 +1,22 @@
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+
+  // sign up the user
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      return db.collection("users").doc(cred.user.uid).set({
+        bio: signupForm["signup-bio"].value,
+      });
+    })
+    .then(() => {
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
+});
