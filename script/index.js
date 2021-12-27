37697f2edb4e18da056ff1f70849ea22b5b222da
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2021-12-27 17:30:41 +0000

    login jorge

--- script/index.js
+++ script/index.js
@@ -1,47 +1,54 @@
+const guideList = document.querySelector(".guides");
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+const accountDetails = document.querySelector(".account-details");
+const setupUI = (user) => {
+  if (user) {
+    db.collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const html = `
+      <div> Logged in as ${user.email}</div>
+      <div>${doc.data().bio}</div>
+      `;
+        accountDetails.innerHTML = html;
+      });
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAfouMJgtqKCfGPdSrrQQ0cPJkDj34DcAA",
-  authDomain: "passenger-26635.firebaseapp.com",
-  databaseURL: "https://passenger-26635-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "passenger-26635",
-  storageBucket: "passenger-26635.appspot.com",
-  messagingSenderId: "857432739466",
-  appId: "1:857432739466:web:8c5f88b01994112c42a773",
-  measurementId: "G-3PKJHYJ5K6"
+    // toggle user UI elements
+    loggedInLinks.forEach((item) => (item.style.display = "block"));
+    loggedOutLinks.forEach((item) => (item.style.display = "none"));
+  } else {
+    accountDetails.innerHTML = ``;
+    // toggle user elements
+    loggedInLinks.forEach((item) => (item.style.display = "none"));
+    loggedOutLinks.forEach((item) => (item.style.display = "block"));
+  }
 };
+// setup guides
+const setupGuides = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML =
+      '<h5 class="center-align">Login to view the Guides </h5>';
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
 
-firebase.initializeApp(irebaseConfi);
-    
-    // make auth and firestore references
-    const db = firebase.firestore();
-
-// Get a reference to the database service
-const database = firebase.getDatabase(app);
-
-const userId = document.getElementById("userId");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const age = document.getElementById("age");
-const addBtn = document.getElementById("addBtn");
-const updateBtn = document.getElementById("updateBtn");
-const removeBtn = document.getElementById("removeBtn");
-
-const database= firebase.database(); 
-
-addBtn.addEventListener('click',  (e) => {
-    e.preventDefault(); 
-    database.ref('/users/'+userId.value).set({
-        first_name: firstName.value,
-        last_name: lastName.value, 
-        age: age.value 
-    })
+  var items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
 });
