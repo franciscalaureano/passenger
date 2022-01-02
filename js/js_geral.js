8c0ed34739f91cafa6134cf9ef80f8c9b96e1dbe
mrjgsantos <58551734+mrjgsantos@users.noreply.github.com>
2022-01-02 21:41:36 +0000

    com as coisas da rafa

--- js/js_geral.js
+++ js/js_geral.js
@@ -1,20 +1,25 @@
 /* APARECER O MENU */
 var menu = document.querySelector(".imagemmenu");
-menu.addEventListener("click", function(evt) {
-    console.log("button click", evt);
-    document.querySelector("nav").classList.toggle("show");
+var map = document.querySelector("#mapid");
+map.style.zIndex = "1";
+
+menu.addEventListener("click", function (evt) {
+  console.log("button click", evt);
+  document.querySelector("nav").classList.toggle("show");
+  map.style.zIndex = "-100";
 });
 
-document.addEventListener("click", function(event) {
-    console.log("document click", event);
-    var isClickInsideButton = menu.contains(event.target);
-    if (!isClickInsideButton) {
-        document.querySelector("nav").classList.remove("show");
-    }
+document.addEventListener("click", function (event) {
+  console.log("document click", event);
+  var isClickInsideButton = menu.contains(event.target);
+  if (!isClickInsideButton) {
+    document.querySelector("nav").classList.remove("show");
+    map.style.zIndex = "1";
+  }
 });
 
 /* RETROCEDER A PARTIR DA SETA */
-var backBtn = document.querySelector('.imagemseta');
-backBtn.addEventListener('click', () => {
-    window.history.back();
+var backBtn = document.querySelector(".imagemseta");
+backBtn.addEventListener("click", () => {
+  window.history.back();
 });
