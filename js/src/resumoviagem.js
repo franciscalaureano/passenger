dd076d8dab2aa86538e4b9a7c84eb9d3cdc4b86d
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-01-11 16:30:42 +0000

    viagens

--- js/src/resumoviagem.js
+++ js/src/resumoviagem.js
@@ -16,15 +16,18 @@ firebase.auth().onAuthStateChanged((user) => {
       data.forEach((doc) => {
         const viagem = doc.data();
         const li = `
+     <a href="https://mysterious-headland-46020.herokuapp.com/d9a7e7cb-6b45-4746-911b-2f7371da1bad" style="margin:auto"/>
+
                 <div class="viagem"> 
                   <div> 
                       <h2>  ${viagem.descricao} </h2>
                       <p class="noMargin">Viagem a <span class="bold">${viagem.onde}</span> </br> com <span class="bold"> ${viagem.com} </span> </p>
                       <p class="noMargin">De <span class="bold">${viagem.inicio}</span> a <span class="bold">${viagem.fim}</span></p>
                   </div>
-                  <a href="https://mysterious-headland-46020.herokuapp.com/d9a7e7cb-6b45-4746-911b-2f7371da1bad" style="margin:auto"/><img id="imagem" src="imagens/entrarviagem.png" width="50" height="50" ></a>
+                  <a href="https://mysterious-headland-46020.herokuapp.com/d9a7e7cb-6b45-4746-911b-2f7371da1bad" style="margin:auto"/>
+                  <img id="imagem" src="imagens/entrarviagem.png" width="50" height="50" > </a>
                 </div>
-                
+               </a> 
                 
               `;
         html += li;
